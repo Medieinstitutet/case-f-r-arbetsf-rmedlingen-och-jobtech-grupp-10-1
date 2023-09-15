3e9f5557be12f566d9760a844a754dc3e6147a76
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-09-15 13:40:06 +0200

    add serviceBase file

---
+++ src/services/serviceBase.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export async function get<T>(url: string) {
+  const response = await axios.get<T>(url);
+
+  return response.data
+}
+
+export async function post<T>(url: string, data: T) {
+  const response = await axios.post<T>(`${url}+${data}`);
+
+  return response.data
+}
+
