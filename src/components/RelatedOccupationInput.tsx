c13f73ea4c02bfbfada2a3d8de606554413b9cf4
alexwallden <alexander.wallden@me.com>
2023-09-17 16:51:28 +0200

    removed react-tagsinput and added react-tag-input-component

--- src/components/RelatedOccupationInput.tsx
+++ src/components/RelatedOccupationInput.tsx
@@ -1,14 +1,16 @@
 // https://www.npmjs.com/package/react-tagsinput
 
-import { useState } from 'react';
-import TagsInput from 'react-tagsinput';
+import { useEffect, useState } from 'react';
+// import TagsInput from 'react-tagsinput';
 import { postSearchQuery } from '../services/relatedOccupationsSearchService';
-import './RelatedOccupationInput.scss'
+import './RelatedOccupationInput.scss';
+import { TagsInput } from 'react-tag-input-component';
 
 const RelatedOccupationInput = () => {
   const [searchWords, setSearchWords] = useState<string[]>([]);
   const [showDuplicateError, setShowDuplicateError] = useState(false);
   const [showLengthError, setShowLengthError] = useState(false);
+  const [pressedOnce, setPressedOnce] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -24,34 +26,83 @@ const RelatedOccupationInput = () => {
     }
   };
 
-  const handleInputChange = (content: string[], updatedContent: string[]) => {
-    setShowDuplicateError(false);
-    setShowLengthError(false);
-    
-    const isDelete = !content.includes(updatedContent[0]);
-    if (isDelete) {
-      setSearchWords((prev) => [
-        ...prev.filter((word) => word !== updatedContent[0]),
-      ]);
-      return;
-    }
+  // const handleInputChange = (content: string[], updatedContent: string[]) => {
+  //   setShowDuplicateError(false);
+  //   setShowLengthError(false);
 
-    const isUnique = !searchWords.includes(updatedContent[0]);
-    if (!isUnique) {
-      setShowDuplicateError(true);
-      return;
-    }
-    setSearchWords((prev) => [...prev, ...updatedContent]);
+  //   const isDelete = !content.includes(updatedContent[0]);
+  //   if (isDelete) {
+  //     setSearchWords((prev) => [
+  //       ...prev.filter((word) => word !== updatedContent[0]),
+  //     ]);
+  //     return;
+  //   }
+
+  //   const isUnique = !searchWords.includes(updatedContent[0]);
+  //   if (!isUnique) {
+  //     setShowDuplicateError(true);
+  //     return;
+  //   }
+  //   setSearchWords((prev) => [...prev, ...updatedContent]);
+  // };
+
+  const handleInputChange = (tags: string[]) => {
+    setSearchWords([...tags]);
   };
 
   const resetErrors = () => {
-    setShowDuplicateError(false);
-    setShowLengthError(false);
+    if (pressedOnce) {
+      setShowDuplicateError(false);
+      setShowLengthError(false);
+      setPressedOnce(false);
+    } else if (showDuplicateError || showLengthError) {
+      console.log('pressedOnce', pressedOnce);
+
+      setPressedOnce(true);
+    }
+  };
+
+  const handleOnExisting = () => {
+    console.log('Inne i on existing');
+
+    setShowDuplicateError(true);
+  };
+
+  const handleOnBlur = (e: any) => {
+    console.log('Inne i on blur');
+    console.log(e.target.onChange);
+    const value = e.target.value;
+    const isDuplicate = searchWords.includes(value);
+    if (isDuplicate) {
+      setShowDuplicateError(true);
+      return;
+    }
+    if (value === '') return;
+    handleInputChange([...searchWords, value]);
+    e.target.value = ''
+    // setSearchWords((prev) => [...prev, e.target.value]);
+    // e.target.value = '';
   }
+
+  useEffect(() => {
+    console.log(searchWords);
+  }, [searchWords]);
 
   return (
     <form onSubmit={handleSubmit}>
       <TagsInput
+        value={searchWords}
+        onChange={(tags) => handleInputChange(tags)}
+        onRemoved={(tag) =>
+          setSearchWords(searchWords.filter((t) => t !== tag))
+        }
+        separators={['Enter', 'Tab', ' ', ',']}
+        onExisting={handleOnExisting}
+        onKeyUp={resetErrors}
+        onBlur={handleOnBlur}
+        placeHolder="Sökord"
+      />
+      {/* <TagsInput
         value={searchWords}
         onChange={(content, updatedContent) =>
           handleInputChange(content, updatedContent)
@@ -61,7 +112,7 @@ const RelatedOccupationInput = () => {
         inputProps={{ placeholder: 'Nytt sökord' }}
         addOnBlur={true}
         renderInput={(props) => <input {...props} onKeyUp={resetErrors}/>}
-      />
+      /> */}
       {showDuplicateError && ( // TODO: Lägg till AFs felmeddelande
         <p style={{ border: 'red solid 2px' }}>
           Du kan inte lägga till samma ord flera gånger
@@ -72,7 +123,7 @@ const RelatedOccupationInput = () => {
           Du måste lägga till minst 3 ord
         </p>
       )}
-      <input type="submit" value='Sök'/>
+      <input type="submit" value="Sök" />
       <button onClick={() => setSearchWords([])}>Rensa</button>
     </form>
   );
