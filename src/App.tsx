805d1083aedcc84b8093d97287da7632eb899ad6
Nicholas Peters <peters.nicholas@gmail.com>
2023-09-18 09:45:09 +0200

    Wrapped RouterProvider in context
    Co-authored-by: Nicholas-Peters <peters.nicholas@gmail.com>
    Co-authored-by: David-Stålgren <david.stalgren@medieinstitutet.se>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.scss';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Router';
+import { RelatedOccupationsReducer } from './reducers/RelatedOccupationsReducer';
+import { useReducer } from 'react';
+import { IMatchByTextResponse } from './models/IMatchByTextResponse';
+import { RelatedOccupationsContext } from './contexts/RelatedOccupationsContext';
+import { RelatedOccupationsDispatchContext } from './contexts/RelatedOccupationsDispatchContext';
 
 function App() {
+  const [occupations, dispatch] = useReducer(
+    RelatedOccupationsReducer,
+    {} as IMatchByTextResponse
+  );
+
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <RelatedOccupationsContext.Provider value={occupations}>
+        <RelatedOccupationsDispatchContext.Provider value={dispatch}>
+          <RouterProvider router={router}></RouterProvider>
+        </RelatedOccupationsDispatchContext.Provider>
+      </RelatedOccupationsContext.Provider>
     </>
   );
 }
