b8cb437ea0c9d8ccac24e23c9cbe59502a0d3576
alexwallden <alexander.wallden@me.com>
2023-09-20 11:53:34 +0200

    add basic tests for serviceBase and relatedOccupationsSearchService

--- jest.config.ts
+++ jest.config.ts
@@ -1,24 +1,13 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest',
-  moduleNameMapper: {
-
-      // if your using tsconfig.paths thers is no harm in telling jest
-    '@components/(.*)$': '<rootDir>/src/components/$1',
-    '@/(.*)$': '<rootDir>/src/$1',
-      
-      // mocking assests and styling
-    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/mocks/fileMock.ts',
-    '^.+\\.(css|less|scss|sass)$': '<rootDir>/src/__tests__/mocks/styleMock.ts',
-    /* mock models and services folder */
-    '(assets|models|services)': '<rootDir>/src/__tests__/mocks/fileMock.ts',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+      "^.+\\.tsx?$": "ts-jest" 
+  // process `*.tsx` files with `ts-jest`
   },
-   // to obtain access to the matchers.
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts'],
-      
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePaths: ['<rootDir>'],
-  modulePathIgnorePatterns: ['<rootDir>/src/__tests__/mocks', '<rootDir>/src/__tests__/setupTests.ts'],
-  testEnvironment: 'jsdom',
-};
+  moduleNameMapper: {
+      '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__test__/__mocks__/fileMock.ts',
+      '^.+\\.(css|less|scss|sass)$': '<rootDir>/src/__test__/__mocks__/styleMock.ts',
+      setupFilesAfterEnv: ["<rootDir>/src/__test__/setupTest.ts"],
+  },
+}
--- src/__tests__/components/Sidebar.test.tsx
+++
@@ -1,5 +0,0 @@
-describe('', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
-});
