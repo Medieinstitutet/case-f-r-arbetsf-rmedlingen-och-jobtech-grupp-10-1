abce9512ba0ec333ac6fc65d22f78d427c2856de
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-09-21 10:33:11 +0200

    make a StyledDigi spinner component

---
+++ src/components/styled/Spinner.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { DigiLoaderSpinner } from '@digi/arbetsformedlingen-react';
+
+export const StyledDigiLoaderSpinner = styled(DigiLoaderSpinner)`
+  svg {
+    color: green;
+  }
+`;
