2a07afb07547cf38b954f1063275dbec7ba5ebd3
Nicholas Peters <peters.nicholas@gmail.com>
2023-09-26 10:12:41 +0200

    remove extra header margin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,10 @@
 import {
   LayoutBlockContainer,
-  LayoutBlockVariation,
   TypographyVariation,
 } from '@digi/arbetsformedlingen';
-import {
-  // DigiIconHome,
-  DigiLayoutBlock,
-  DigiTypography,
-} from '@digi/arbetsformedlingen-react';
+import { DigiTypography } from '@digi/arbetsformedlingen-react';
 import { Link } from 'react-router-dom';
-import { Header as StyledHeader} from './styled/Header';
+import { Header as StyledHeader } from './styled/Header';
 
 export const Header = () => {
   return (
@@ -17,13 +12,11 @@ export const Header = () => {
       <StyledHeader
         afContainer={LayoutBlockContainer.FLUID}
         afVerticalPadding={true}
-        afMarginBottom={true}
       >
         <DigiTypography afVariation={TypographyVariation.LARGE}>
           <Link to={'/'}>
             <h1 className="headerH1">YrkesOraklet</h1>
           </Link>
-          {/* <DigiIconHome /> */}
         </DigiTypography>
       </StyledHeader>
     </header>
